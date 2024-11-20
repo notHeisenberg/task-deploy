90862b6dff84e78ea31de3f96fcb5c3145d90aec
mr_nobody <mahfuzantor2@gmail.com>
2024-11-20 20:27:45 +0600

    Update Stripe pricing data with new price IDs

--- src/stripe/pricingData.ts
+++ src/stripe/pricingData.ts
@@ -2,7 +2,7 @@ import { Price } from "@/types/price";
 
 export const pricingData: Price[] = [
   {
-    id: "price_1NQk5TLtGdPVhGLecVfQ7mn0",
+    id: "price_1QNElrHgui4O3QJ2XYNkI9Ma",
     unit_amount: 100 * 100,
     nickname: "Basic",
     offers: [
@@ -15,7 +15,7 @@ export const pricingData: Price[] = [
     ],
   },
   {
-    id: "price_1NQk55LtGdPVhGLefU8AHqHr",
+    id: "price_1QNEnIHgui4O3QJ22jm4pmPZ",
     unit_amount: 200 * 100,
     nickname: "Premium",
     offers: [
@@ -28,7 +28,7 @@ export const pricingData: Price[] = [
     ],
   },
   {
-    id: "price_1NQk4eLtGdPVhGLeZsZDsCNz",
+    id: "price_1QNEmWHgui4O3QJ2pfBoEcfU",
     unit_amount: 300 * 100,
     nickname: "Business",
     offers: [
